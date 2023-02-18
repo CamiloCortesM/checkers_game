75bf984ee28373164ef3acd9c65ad6dc92c3d735
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-02-18 12:08:41 -0500

    initialized the other game modes and corrected the end-of-game interface

--- Game.py
+++ Game.py
@@ -2,20 +2,130 @@ import pygame
 import cfg
 from modules import *
 
+def choose_game(screen,cfg):
+    board = Board(8)
+    screen.fill((255,255,255))
+    font_title = pygame.font.Font(cfg.FONTPATH, 45)
+    font = pygame.font.Font(cfg.FONTPATH, 25)
+    tfont = font_title.render("Choose game mode:",True,(255,0,0))
+    cfont1 = font.render("Enter 1 for play human vs AI",True,(0,0,0))
+    cfont2 = font.render("Enter 2 for play human vs human",True,(0,0,0))
+    cfont3 = font.render("Enter 3 for play AI vs AI",True,(0,0,0))
+    cfont4 = font.render("Enter 4 for play ONLINE",True,(0,0,0))
+    
+    trect = tfont.get_rect()
+    crect1 = cfont1.get_rect()
+    crect2 = cfont2.get_rect()
+    crect3 = cfont3.get_rect()
+    crect4 = cfont4.get_rect()
+    
+    trect.midtop = (cfg.SCREENSIZE[0]/2,cfg.SCREENSIZE[1]/5)
+    crect1.midtop = (cfg.SCREENSIZE[0]/2,cfg.SCREENSIZE[1]/2.5)
+    crect2.midtop = (cfg.SCREENSIZE[0]/2,cfg.SCREENSIZE[1]/2)
+    crect3.midtop = (cfg.SCREENSIZE[0]/2,cfg.SCREENSIZE[1]/1.66)
+    crect4.midtop = (cfg.SCREENSIZE[0]/2,cfg.SCREENSIZE[1]/1.42)
+    
+    screen.blit(tfont,trect)
+    screen.blit(cfont1,crect1)
+    screen.blit(cfont2,crect2)
+    screen.blit(cfont3,crect3)
+    screen.blit(cfont4,crect4)
+    
+    while True:  
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    game_play_human_vs_ai(screen,board)
+                elif event.key == pygame.K_2:
+                    game_play_human(screen,board)         
+                elif event.key == pygame.K_3:
+                    game_play_ai_vs_ai(screen,board)           
+                elif event.key == pygame.K_4:
+                    return 4           
+        pygame.display.update()
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(cfg.SCREENSIZE)
     pygame.display.set_caption("Damas Inglesas")
-    board = Board(8)
-    game = choose_game(screen,cfg)
-    print(game)
-    # game_play_human(screen,board)
+    choose_game(screen,cfg)
       
+    
+def game_play_ai_vs_ai(screen,board):
+    my_color,opponent_color = 'red','white'
+    initialize(board,my_color)
     
 def game_play_human_vs_ai(screen,board):
     (my_color, opponent_color) = tools.choose_color(screen,cfg)
     turn = my_color if my_color == 'red' else opponent_color
     initialize(board,my_color)
+    piece_selected = None
+    moves = []
+    piece_count = None
+    move_origin = None
+    pre_moves = []
+    jumps = []
+    while not is_game_finished(board,my_color):
+        if turn == opponent_color:
+            print("Move AI")
+            turn = my_color
+            continue
+        jumps = find_jump(board,my_color,turn)
+        if len(jumps)>0 and moves==[]: 
+            for jump in jumps:    
+                move_origin,moves_jumps = jump
+                for mv in moves_jumps:
+                    moves.append(mv)
+                    pre_moves.append([move_origin,mv])
+            move_origin = None
+        screen.fill((0,0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x, y = pygame.mouse.get_pos()
+                x_pos = x//board.get_cell_size()
+                y_pos = (y-20)//(board.get_cell_size()) 
+                row, col = y_pos, x_pos
+                if not board.is_free(row,col) and len(jumps)==0:
+                    piece_selected = board.get(row,col)
+                    if piece_selected.color() == turn:
+                        move_origin = (row,col)
+                        moves = get_moves(board,row,col,my_color)
+                    else:
+                        piece_selected = None
+                if piece_selected and board.is_free(row,col):
+                    move = [move_origin,(row,col)]
+                    if apply_move(board,move,my_color):  
+                        piece_selected = None
+                        move_origin = None
+                        moves = []
+                        turn = "red" if turn == "white" else "white"
+                    elif apply_capture(board,move,my_color):
+                        piece_selected = None
+                        move_origin = None
+                        moves = []
+                        turn = "red" if turn == "white" else "white" 
+                if len(jumps)>0:
+                    if (row,col) in moves:
+                        for move_jump in pre_moves:
+                            if move_jump[1] == (row,col):
+                               move = move_jump
+                        if apply_capture(board,move,my_color):
+                           moves = []
+                           pre_moves = []
+                           jumps = get_jumps(board,row,col,my_color)
+                           if len(jumps)>0:
+                               break
+                           turn = "red" if turn == "white" else "white"       
+        board.draw(screen)
+        piece_count = count_pieces(board)   
+        board.display(screen,cfg,turn,piece_count)
+        if moves: board.draw_moves(moves,screen)
+        pygame.display.update()
+    endInterface(screen,get_winner(board),cfg)     
 
 def game_play_human(screen,board):
     (my_color, opponent_color) = ("red","white")
--- modules/controller.py
+++ modules/controller.py
@@ -1,7 +1,8 @@
 from .piece import Piece
-from .tools import get_moves,get_captures,get_jumps
+from .tools import get_moves, get_captures, get_jumps
 
-def initialize(board,mycolor):
+
+def initialize(board, mycolor):
     row = col = board.get_length()
     initrows = (row // 2) - 1
     for r in range(row - 1, row - (initrows + 1), -1):
@@ -9,20 +10,22 @@ def initialize(board,mycolor):
             board.place(r, c, Piece(mycolor))
     for r in range(0, initrows):
         for c in range(0 if r % 2 == 1 else 1, col, 2):
-            board.place(r, c, Piece('white' if mycolor=='red' else 'red'))
-            
-def find_jump(board,my_color,turn):
+            board.place(r, c, Piece('white' if mycolor == 'red' else 'red'))
+
+
+def find_jump(board, my_color, turn):
     jumps = []
     for row in range(8):
-            for col in range(8):
-                piece_selected = board.get(row,col)
-                if piece_selected:
-                    if piece_selected.color() == turn:
-                        jump = get_jumps(board,row,col,my_color)
-                        if len(jump)>0:
-                            jumps.append([(row,col),jump])
+        for col in range(8):
+            piece_selected = board.get(row, col)
+            if piece_selected:
+                if piece_selected.color() == turn:
+                    jump = get_jumps(board, row, col, my_color)
+                    if len(jump) > 0:
+                        jumps.append([(row, col), jump])
     return jumps
-                
+
+
 def count_pieces(board):
     row = col = board.get_length()
     red, white = 0, 0
@@ -36,7 +39,8 @@ def count_pieces(board):
                     white += 1
     return (red, white)
 
-def get_all_moves(board, color,mycolor, is_sorted = False,):
+
+def get_all_moves(board, color, mycolor, is_sorted=False,):
     row = col = board.get_length()
     final_list = []
     for r in range(row):
@@ -44,16 +48,17 @@ def get_all_moves(board, color,mycolor, is_sorted = False,):
             piece = board.get(r, c)
             if piece:
                 if piece.color() == color:
-                    path_list = get_moves(board, r, c, is_sorted,mycolor)
+                    path_list = get_moves(board, r, c, is_sorted, mycolor)
                     path_start = (r, c)
                     for path in path_list:
                         final_list.append((path_start, path))
-    
+
     if is_sorted == True:
         final_list.sort()
     return final_list
 
-def get_all_captures(board, color,mycolor, is_sorted = False):
+
+def get_all_captures(board, color, mycolor, is_sorted=False):
     row = col = board.get_length()
     final_list = []
     for r in range(row):
@@ -61,50 +66,52 @@ def get_all_captures(board, color,mycolor, is_sorted = False):
             piece = board.get(r, c)
             if piece:
                 if piece.color() == color:
-                    path_list = get_captures(board, r, c,mycolor, is_sorted)
+                    path_list = get_captures(board, r, c, mycolor, is_sorted)
                     for path in path_list:
-                        final_list.append(path) 
-    return sorted(final_list, key = lambda x: (-len(x), x[0]))\
-    if is_sorted else final_list
-    
-def apply_move(board, move,my_color):
-    row,col = (move[0])
-    row_end,col_end = (move[1])
-    path_list = get_moves(board, row, col,my_color, is_sorted = False)
+                        final_list.append(path)
+    return sorted(final_list, key=lambda x: (-len(x), x[0]))\
+        if is_sorted else final_list
+
+
+def apply_move(board, move, my_color):
+    row, col = (move[0])
+    row_end, col_end = (move[1])
+    path_list = get_moves(board, row, col, my_color, is_sorted=False)
     if move[1] in path_list:
         piece = board.get(row, col)
         if my_color == "white":
             if piece.is_red() and row_end == board.get_length()-1 \
-            or piece.is_white() and row_end == 0:
+                    or piece.is_white() and row_end == 0:
                 piece.turn_king()
         else:
             if piece.is_white() and row_end == board.get_length()-1 \
-            or piece.is_red() and row_end == 0:
+                    or piece.is_red() and row_end == 0:
                 piece.turn_king()
         board.remove(row, col)
         board.place(row_end, col_end, piece)
         return True
     else:
         return False
-        
-def apply_capture(board, capture_path,my_color):
+
+
+def apply_capture(board, capture_path, my_color):
     counter = 0
     while counter < len(capture_path)-1:
         path = [capture_path[counter], capture_path[counter + 1]]
         counter += 1
-        row,col = (path[0])
-        row_end,col_end = (path[1])
-        path_list = get_jumps(board, row, col,my_color, is_sorted = False)
-        
+        row, col = (path[0])
+        row_end, col_end = (path[1])
+        path_list = get_jumps(board, row, col, my_color, is_sorted=False)
+
         if path[1] in path_list:
             piece = board.get(row, col)
             if my_color == "white":
                 if piece.is_red() and row_end == board.get_length()-1 \
-                or piece.is_white() and row_end == 0:
+                        or piece.is_white() and row_end == 0:
                     piece.turn_king()
             else:
                 if piece.is_white() and row_end == board.get_length()-1 \
-                or piece.is_red() and row_end == 0:
+                        or piece.is_red() and row_end == 0:
                     piece.turn_king()
             board.remove(row, col)
             row_eat, col_eat = max(row, row_end)-1, max(col, col_end)-1
@@ -114,51 +121,47 @@ def apply_capture(board, capture_path,my_color):
         else:
             return False
 
-def get_hints(board, color,mycolor, is_sorted = False):
-    move = get_all_moves(board, color,mycolor, is_sorted)
-    jump = get_all_captures(board, color,mycolor, is_sorted)
+
+def get_hints(board, color, mycolor, is_sorted=False):
+    move = get_all_moves(board, color, mycolor, is_sorted)
+    jump = get_all_captures(board, color, mycolor, is_sorted)
     if jump:
         return ([], jump)
     else:
         return (move, jump)
 
-def get_winner(board, is_sorted = False):
-    red_hint = get_hints(board, 'red', is_sorted)
-    white_hint = get_hints(board, 'white', is_sorted)
-    if red_hint != ([],[]) and white_hint == ([],[]):
-        return 'red'
-    elif red_hint == ([],[]) and white_hint != ([],[]):
-        return 'white'
-    else:
-        red_king,white_king = 0,0
-        red, white = 0,0
-        row = col = board.get_length()
-        for r in range(row):
-            for c in range(col):
-                piece = board.get(r, c)
-                if piece:
-                    if piece.is_red():
-                        red += 1
-                        if piece.is_king():
-                            red_king += 1
-                    else:
-                        white += 1
-                        if piece.is_king():
-                            white_king += 1
-        if white_king == 1 and red_king == 1 and white == 1 and red == 1:
-            return 'draw'
-        else:
-            if white > red:
-                return 'white'
-            elif red > white:
-                return 'red'
-            else:
-                return 'draw'
 
-def is_game_finished(board,mycolor, is_sorted = False):
-    red_hint = get_hints(board, 'red', is_sorted,mycolor)
-    white_hint = get_hints(board, 'white', is_sorted,mycolor)
-    if red_hint == ([],[]) or white_hint == ([],[]):
+def get_winner(board, is_sorted=False):
+    red_king, white_king = 0, 0
+    red, white = 0, 0
+    row = col = board.get_length()
+    for r in range(row):
+        for c in range(col):
+            piece = board.get(r, c)
+            if piece:
+                if piece.is_red():
+                    red += 1
+                    if piece.is_king():
+                        red_king += 1
+                else:
+                    white += 1
+                    if piece.is_king():
+                        white_king += 1
+    if white_king == 1 and red_king == 1 and white == 1 and red == 1:
+        return 'draw'
+    else:
+        if white > red:
+            return 'white'
+        elif red > white:
+            return 'red'
+        else:
+            return 'draw'
+
+
+def is_game_finished(board, mycolor, is_sorted=False):
+    red_hint = get_hints(board, 'red', is_sorted, mycolor)
+    white_hint = get_hints(board, 'white', is_sorted, mycolor)
+    if red_hint == ([], []) or white_hint == ([], []):
         return True
     else:
         return False
